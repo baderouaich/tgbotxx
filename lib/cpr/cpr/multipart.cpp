6fba7a249de6a961a689dbd0f0667f820c5083ff
Bader <badereddineouaich@gmail.com>
2023-09-22 00:30:43 +0100

    now using CMake's FetchContent to download libraries (if not already installed)

--- lib/cpr/cpr/multipart.cpp
+++
@@ -1,7 +0,0 @@
-#include "cpr/multipart.h"
-
-namespace cpr {
-Multipart::Multipart(const std::initializer_list<Part>& p_parts) : parts{p_parts} {}
-Multipart::Multipart(const std::vector<Part>& p_parts) : parts{p_parts} {}
-Multipart::Multipart(const std::vector<Part>&& p_parts) : parts{p_parts} {}
-} // namespace cpr
