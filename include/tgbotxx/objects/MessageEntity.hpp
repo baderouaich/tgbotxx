7c648b7803602792e1367591ad0ef66ad3170600
Bader <badereddineouaich@gmail.com>
2023-09-22 23:26:50 +0100

    added object MessageEntity and its JSON serialization Still, I am not satisfied with this object implementation and the way it converts Enum to String and vice versa.

--- include/tgbotxx/objects/MessageEntity.hpp
+++ include/tgbotxx/objects/MessageEntity.hpp
@@ -1,8 +1,8 @@
 #pragma once
-#include "Object.hpp"
+#include <tgbotxx/objects/Object.hpp>
+#include <tgbotxx/objects/User.hpp>
 #include <cstdint>
 #include <string>
-#include "User.hpp"
 
 namespace tgbotxx {
     /// @brief This object represents one special entity in a text message. For example, hashtags, usernames, URLs, etc.
@@ -11,7 +11,7 @@ namespace tgbotxx {
         /// @brief Enum of possible types of MessageEntity.
         enum class Type : std::uint8_t {
             /// @brief “mention” (@username)
-            Mention,
+            Mention = 0,
             /// @brief “hashtag” (#hashtag)
             Hashtag,
             /// @brief “cashtag” ($USD)
@@ -45,6 +45,35 @@ namespace tgbotxx {
             /// @brief “custom_emoji” (for inline custom emoji stickers)
             CustomEmoji
         };
+        inline static constexpr const char* Type2Str[] = {
+                "mention",
+                "hashtag",
+                "cashtag",
+                "bot_command",
+                "url",
+                "email",
+                "phone_number",
+                "bold",
+                "italic",
+                "underline",
+                "strikethrough",
+                "spoiler",
+                "code",
+                "pre",
+                "text_link",
+                "text_mention",
+                "custom_emoji"
+        };
+
+        inline static constexpr Type Str2Type(const std::string& str){
+          std::uint8_t i = 0;
+          for(const char* v : Type2Str) {
+            if(str == v)
+              return static_cast<Type>(i);
+            ++i;
+          }
+          throw Exception("Could not convert string \""+str+"\" to enum Type");
+        }
 
         /// @brief Type of the entity.
         /// Currently, can be “mention” (@username), “hashtag” (#hashtag), “cashtag” ($USD),
@@ -74,13 +103,29 @@ namespace tgbotxx {
         /// Use Api::getCustomEmojiStickers to get full information about the sticker
         std::string customEmojiId;
 
-        // TODO: implement fromJson toJson
+        /// @brief Serializes this object to JSON
+        /// @returns JSON representation of this object
         nl::json toJson() const {
-          return nl::json();
+          nl::json json = nl::json::object();
+          json["type"] = Type2Str[static_cast<std::size_t>(type)]; // enum
+          OBJECT_SERIALIZE_FIELD(json, "offset", offset);
+          OBJECT_SERIALIZE_FIELD(json, "length", length);
+          OBJECT_SERIALIZE_FIELD(json, "url", url);
+          OBJECT_SERIALIZE_FIELD_PTR(json, "user", user, nl::json::object());
+          OBJECT_SERIALIZE_FIELD(json, "language", language);
+          OBJECT_SERIALIZE_FIELD(json, "custom_emoji_id", customEmojiId);
+          return json;
         }
 
+        /// @brief Deserializes this object from JSON
         void fromJson(const nl::json &json) {
-
+          type = Str2Type(json["type"]); // enum
+          OBJECT_DESERIALIZE_FIELD(json, "offset", offset, 0, false);
+          OBJECT_DESERIALIZE_FIELD(json, "length", length, 0, false);
+          OBJECT_DESERIALIZE_FIELD(json, "url", url, "", true);
+          OBJECT_DESERIALIZE_FIELD_PTR(json, "user", user, true);
+          OBJECT_DESERIALIZE_FIELD(json, "language", language, "", true);
+          OBJECT_DESERIALIZE_FIELD(json, "custom_emoji_id", customEmojiId, "", true);
         }
     };
 }
