2c11581b919763d47c9a3d773295b495c691a071
Bader <badereddineouaich@gmail.com>
2023-09-22 14:28:22 +0100

    updated tgbotxx.hpp

---
+++ include/tgbotxx/tgbotxx.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <tgbotxx/Api.hpp>
+#include <tgbotxx/Bot.hpp>
+
+/// Objects
+#include <tgbotxx/objects/Command.hpp>
+#include <tgbotxx/objects/Message.hpp>
+#include <tgbotxx/objects/Update.hpp>
+#include <tgbotxx/objects/User.hpp>
+
+/// Utils
+#include <tgbotxx/utils/StringUtils.hpp>
+#include <tgbotxx/utils/Ptr.hpp>
