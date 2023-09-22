4bfe1b0f57f754cb43e6a38090c6df7db2917b00
Bader <badereddineouaich@gmail.com>
2023-09-22 23:24:12 +0100

    Added 'optional' field to object serializer macros when field is optional

--- include/tgbotxx/objects/Object.hpp
+++ include/tgbotxx/objects/Object.hpp
@@ -1,27 +1,54 @@
 #pragma once
-#include <nlohmann/json.hpp>
+/// Available objects: https://core.telegram.org/bots/api#available-types
+#include <cstdint>
+#include <string>
+#include <memory>
 #include <tgbotxx/utils/Ptr.hpp>
-#include <type_traits>
+#include <tgbotxx/Exception.hpp>
 #include <iostream>
+#include <nlohmann/json.hpp>
 namespace nl = nlohmann;
 
 /// @brief Helper macros to de/serialize objects
-#define OBJECT_SERIALIZE_FIELD(json, json_field, field)  \
-      json[json_field] = field;
+#define OBJECT_SERIALIZE_FIELD(json, json_field, field) \
+  json[json_field] = field;
 
-
-#define OBJECT_SERIALIZE_FIELD_OPT(json, json_field, field, default_value)  \
+#define OBJECT_SERIALIZE_FIELD_PTR(json, json_field, field, default_value) \
   json[json_field] = field ? field->toJson() : default_value
 
-#define OBJECT_DESERIALIZE_FIELD(json, json_field, field, default_value)                 \
-  if (json.contains(json_field)) {                                               \
-    using T = std::remove_reference_t<std::remove_const_t<decltype(field)>>; \
-    field = json[json_field].get<T>();                                           \
-  } else { field = default_value; std::cerr << "Could not deserialize field \""<<json_field<<"\" from json object: " << json.dump(2) << std::endl;  }
+#define OBJECT_DESERIALIZE_FIELD(json, json_field, field, default_value, optional)                                       \
+  if (json.contains(json_field))                                                                                         \
+  {                                                                                                                      \
+    using T = std::remove_reference_t<std::remove_const_t<decltype(field)>>;                                             \
+    field = json[json_field].get<T>();                                                                                   \
+  }                                                                                                                      \
+  else                                                                                                                   \
+  {                                                                                                                      \
+    if(not (optional))                                                                                                   \
+    {                                                                                                                    \
+      std::ostringstream err{};                                                                                          \
+      err << __FILE__ << ':' << __LINE__ <<": "<<__PRETTY_FUNCTION__<<": Missing required field \""                      \
+      << json_field << "\" from json object: " << json.dump(2);                                                          \
+      throw Exception(err.str());                                                                                        \
+    }                                                                                                                    \
+    field = default_value;                                                                                               \
+  }
 
-#define OBJECT_DESERIALIZE_FIELD_OPT(json, json_field, field)                            \
-  if (json.contains(json_field)) {                                               \
-    using T = std::remove_reference_t<std::remove_const_t<decltype(field)>>; \
-    using E = T::element_type;                                               \
-    field.reset(new (E)( json[json_field] ));                           \
-  } else { field.reset(); std::cerr << "Could not deserialize field \""<<json_field<<"\" from json object: " << json.dump(2) << std::endl;  }
+#define OBJECT_DESERIALIZE_FIELD_PTR(json, json_field, field, optional)                                                  \
+  if (json.contains(json_field))                                                                                         \
+  {                                                                                                                      \
+    using T = std::remove_reference_t<std::remove_const_t<decltype(field)>>;                                             \
+    using E = T::element_type;                                                                                           \
+    field.reset(new (E)(json[json_field]));                                                                              \
+  }                                                                                                                      \
+  else                                                                                                                   \
+  {                                                                                                                      \
+    if(not (optional))                                                                                                   \
+    {                                                                                                                    \
+      std::ostringstream err{};                                                                                          \
+      err << __FILE__ << ':' << __LINE__ <<": "<<__PRETTY_FUNCTION__<<": Missing required field \""                      \
+      << json_field << "\" from json object: " << json.dump(2);                                                          \
+      throw Exception(err.str());                                                                                        \
+    }                                                                                                                    \
+    field = nullptr;                                                                                                     \
+  }
