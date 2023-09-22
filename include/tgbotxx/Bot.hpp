fda4b12567e2075b61a4716e2dac975d44653757
Bader <badereddineouaich@gmail.com>
2023-09-22 18:07:21 +0100

    Updated Bot made callbacks non pure virtual so users can ignore callbacks they dont want.

--- include/tgbotxx/Bot.hpp
+++ include/tgbotxx/Bot.hpp
@@ -6,7 +6,7 @@
 
 namespace tgbotxx {
     class Message;
-    class Command;
+    class BotCommand;
     class Update;
 
     class Bot {
@@ -14,6 +14,7 @@ namespace tgbotxx {
         Ptr<Api> m_api;
         //Ptr<std::thread> m_thread;
         std::atomic<bool> m_running;
+        std::int32_t m_lastUpdateId;
 
     public:
         Bot(const std::string& token);
@@ -22,21 +23,33 @@ namespace tgbotxx {
         void stop();
         void restart();
 
-    public: /** Bot Callbacks */
-        /**
-         * @brief Called when a new message is received of any kind - text, photo, sticker, etc.
-         * @param message Received message object
-         */
-        virtual void onAnyMessage(const Ptr<Message>& message) = 0;
+    public: /// Bot Callbacks
+        /// @brief Called when a new message is received of any kind - text, photo, sticker, etc.
+        /// @param message Received message object
+        virtual void onAnyMessage(const Ptr<Message>& message) {}
 
-        /**
-         * @brief Called when a new command is received (messages with leading '/' char).
-         * @param command Received command object
-         */
-        virtual void onCommand(const Ptr<Command>& command) = 0;
+        /// @brief Called when a non-command message is received of any kind - text, photo, sticker, etc.
+        /// @param message Received non-command message object
+        virtual void onNonCommandMessage(const Ptr<Message>& message) {}
 
-    protected:
+        /// @brief Called when a new command is received (messages with leading '/' char).
+        /// @param command Received command object
+        virtual void onCommand(const Ptr<Message>& command) {}
+
+        /// @brief Called when an unknown command is received (messages with leading '/' char).
+        /// @param message Received unknown command object
+        /// @note Known commands are set with Bot::setCommands()
+        virtual void onUnknownCommand(const Ptr<Message>& message) {}
+
+    protected: /// Setters
+
+    protected: /// Getters
         const Ptr<Api>& getApi() const noexcept;
+
+    private:
+        /// @brief Dispatch update to callbacks
+        /// @param update: Update to dispatch
+        void dispatch(const Ptr<Update>& update);
     };
 
 }
