6a09deb11f2b1eb339d32b7030e44e5bd1944a8e
Bader <badereddineouaich@gmail.com>
2023-09-22 18:08:04 +0100

    updated api

--- src/Api.cpp
+++ src/Api.cpp
@@ -1,10 +1,13 @@
 #include <tgbotxx/Api.hpp>
 #include <tgbotxx/Exception.hpp>
 #include <tgbotxx/objects/Update.hpp>
+#include <tgbotxx/objects/BotCommand.hpp>
+#include <tgbotxx/objects/BotCommandScope.hpp>
+#include <utility>
 #include <cpr/cpr.h>
 using namespace tgbotxx;
 
-Api::Api(const std::string& token) : m_token(token) {
+Api::Api(const std::string& token) : m_token(std::move(token)) {
 
 }
 
@@ -56,24 +59,61 @@ bool Api::deleteWebhook(bool dropPendingUpdates) const {
   cpr::Multipart data{};
   if(dropPendingUpdates)
     data.parts.emplace_back("dropPendingUpdates", dropPendingUpdates);
-  nl::json res = sendRequest("deleteWebhook", data);
-  std::cout << __func__ << ": " << res.dump(2) << std::endl;
-  return res.get<bool>();
+  return sendRequest("deleteWebhook", data).get<bool>();
 }
 
-std::vector<Ptr<Update>> Api::getUpdates() const {
-  cpr::Multipart data = { // TODO
-          {"offset", ""},
-          {"limit", ""},
-          {"timeout", ""},
-          {"allowed_updates", ""},
+/// Called every LONG_POOL_TIMEOUT seconds
+std::vector<Ptr<Update>> Api::getUpdates(std::int32_t offset, std::int32_t limit, std::int32_t timeout, const std::vector<std::string> &allowedUpdates) const {
+  cpr::Multipart data = {
+          {"offset", offset},
+          {"limit", std::max(1, std::min(100, limit))},
+          {"timeout", timeout},
+          {"allowed_updates", nl::json{allowedUpdates}.dump()},
   };
   nl::json updatesJson = sendRequest("getUpdates", data);
   std::vector<Ptr<Update>> updates;
   updates.reserve(updatesJson.size());
-  for(const nl::json& updateObj : updatesJson){
-    //Ptr<Update> update(new Update(updateObj));
-    //updates.push_back(std::move(update));
+  for(const nl::json& updateObj : updatesJson) {
+    Ptr<Update> update(new Update(updateObj));
+    updates.push_back(std::move(update));
   }
   return updates;
+}
+
+bool Api::setMyCommands(const std::vector<Ptr<BotCommand>> &commands, const Ptr<BotCommandScope>& scope, const std::string &languageCode) const {
+  cpr::Multipart data{};
+  data.parts.reserve(3);
+
+  nl::json commandsJson = nl::json::array();
+  for(const Ptr<BotCommand>& command : commands)
+      commandsJson.push_back(command->toJson());
+  data.parts.emplace_back("commands", commandsJson.dump());
+  if (scope) {
+    data.parts.emplace_back("scope", scope->toJson().dump());
+  }
+  if (not languageCode.empty()) {
+    data.parts.emplace_back("language_code", languageCode);
+  }
+
+  return sendRequest("setMyCommands", data).get<bool>();
+}
+
+std::vector<Ptr<BotCommand>> Api::getMyCommands(const Ptr<BotCommandScope> &scope, const std::string &languageCode) const {
+  std::vector<Ptr<BotCommand>> commands;
+  cpr::Multipart data{};
+  data.parts.reserve(2);
+  if (scope) {
+    data.parts.emplace_back("scope", scope->toJson().dump());
+  }
+  if (not languageCode.empty()) {
+    data.parts.emplace_back("language_code", languageCode);
+  }
+  nl::json commandsJson = sendRequest("getMyCommands", data);
+  if(commandsJson.empty()) return commands;
+  commands.reserve(commandsJson.size());
+  for(const nl::json& commandObj : commandsJson) {
+    Ptr<BotCommand> cmd(new BotCommand(commandObj));
+    commands.push_back(std::move(cmd));
+  }
+  return commands;
 }
