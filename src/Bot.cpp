cb71b9b384817462c6ec4f6584c3066f1d49613a
Bader <badereddineouaich@gmail.com>
2023-09-22 18:08:40 +0100

    updated Bot now run function is long polling every 10 seconds for updates then updates are dispatched and callbacks are called

--- src/Bot.cpp
+++ src/Bot.cpp
@@ -1,12 +1,13 @@
 #include <tgbotxx/Bot.hpp>
-#include <tgbotxx/objects/Command.hpp>
+#include <tgbotxx/objects/BotCommand.hpp>
 #include <tgbotxx/objects/Message.hpp>
 #include <tgbotxx/objects/Update.hpp>
 #include <tgbotxx/objects/User.hpp>
+#include <tgbotxx/utils/StringUtils.hpp>
 using namespace tgbotxx;
 
 Bot::Bot(const std::string& token)
-  : m_token(token), m_api(new Api(token)), /*m_thread(nullptr),*/ m_running(false) {
+  : m_token(token), m_api(new Api(token)), /*m_thread(nullptr),*/ m_running(false), m_lastUpdateId(0) {
 }
 
 void Bot::start() {
@@ -15,8 +16,16 @@ void Bot::start() {
   m_running.store(true, std::memory_order::relaxed);
   while(m_running)
   {
-    std::cout << "Getting updates...\n";
-    std::vector<Ptr<Update>> updates = m_api->getUpdates();
+    std::cout << "Getting updates (long poll)...\n";
+    std::vector<Ptr<Update>> updates = m_api->getUpdates(/*offset=*/m_lastUpdateId);
+    std::cout << "Got " << updates.size() << " updates\n";
+    for (const Ptr<Update>& update : updates) {
+      if (update->updateId >= m_lastUpdateId) {
+        m_lastUpdateId = update->updateId + 1;
+      }
+      this->dispatch(update);
+    }
+
   }
 //  m_thread.reset(new std::thread([this]{
 //      while(m_running) {
@@ -35,11 +44,56 @@ void Bot::stop() {
 //  }
 }
 
-const Ptr<Api>& Bot::getApi() const noexcept {
-  return m_api;
-}
 
 void Bot::restart() {
   stop();
   start();
+}
+
+void Bot::dispatch(const Ptr<Update> &update) {
+  std::cout << "Dispatching update " << update->updateId << "..."<<std::endl;
+
+  if(update->message) {
+    const Ptr<Message>& message = update->message;
+
+    /// Callback -> onAnyMessage
+    this->onAnyMessage(message);
+
+    if (StringUtils::startsWith(message->text, "/")) {
+      std::size_t splitPosition;
+      std::size_t spacePosition = message->text.find(' ');
+      std::size_t atSymbolPosition = message->text.find('@');
+      if (spacePosition == std::string::npos) {
+        if (atSymbolPosition == std::string::npos) {
+          splitPosition = message->text.size();
+        } else {
+          splitPosition = atSymbolPosition;
+        }
+      } else if (atSymbolPosition == std::string::npos) {
+        splitPosition = spacePosition;
+      } else {
+        splitPosition = std::min(spacePosition, atSymbolPosition);
+      }
+      std::string command = message->text.substr(1, splitPosition - 1);
+      std::vector<Ptr<BotCommand>> myCommands = m_api->getMyCommands();
+      bool isKnownCommand = std::any_of(myCommands.begin(), myCommands.end(), [&command](const Ptr<BotCommand>& cmd) {
+         return cmd->command == command;
+      });
+      if (isKnownCommand) {
+        /// Callback -> onCommand
+        this->onCommand(message);
+      }
+      else {
+        /// Callback -> onUnknownCommand
+        this->onUnknownCommand(message);
+      }
+    } else {
+      /// Callback -> onNonCommandMessage
+      this->onNonCommandMessage(message);
+    }
+  }
+}
+
+const Ptr<Api>& Bot::getApi() const noexcept {
+  return m_api;
 }
