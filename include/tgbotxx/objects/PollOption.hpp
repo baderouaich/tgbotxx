748ab5b5f90cc2dfd08dbb541ecfedcee6342fbc
Bader <badereddineouaich@gmail.com>
2023-09-23 18:30:13 +0100

    added object PollOption object with its JSON serialization and documentation

---
+++ include/tgbotxx/objects/PollOption.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <tgbotxx/objects/Object.hpp>
+
+namespace tgbotxx {
+    /// @brief This object contains information about one answer option in a poll.
+    /// @ref https://core.telegram.org/bots/api#polloption
+    struct PollOption {
+        explicit PollOption(const nl::json& json) {
+          fromJson(json);
+        }
+
+        /// @brief Option text, 1-100 characters
+        std::string text;
+
+        /// @brief Number of users that voted for this option
+        std::int32_t voterCount;
+
+        /// @brief Serializes this object to JSON
+        /// @returns JSON representation of this object
+        nl::json toJson() const {
+          nl::json json = nl::json::object();
+          OBJECT_SERIALIZE_FIELD(json, "text", text);
+          OBJECT_SERIALIZE_FIELD(json, "voter_count", voterCount);
+          return json;
+        }
+
+        /// @brief Deserializes this object from JSON
+        void fromJson(const nl::json &json) {
+          OBJECT_DESERIALIZE_FIELD(json, "text", text, "", false);
+          OBJECT_DESERIALIZE_FIELD(json, "voter_count", voterCount, 0, false);
+        }
+    };
+}
