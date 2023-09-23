f8adba0d12f9f4657766394f4fe198192b556d11
Bader <badereddineouaich@gmail.com>
2023-09-23 18:31:27 +0100

    added helper macro OBJECT_DESERIALIZE_FIELD_PTR_ARRAY to deserialize array of ptr objects

--- include/tgbotxx/objects/Object.hpp
+++ include/tgbotxx/objects/Object.hpp
@@ -2,7 +2,9 @@
 /// Available objects: https://core.telegram.org/bots/api#available-types
 #include <cstdint>
 #include <string>
+#include <sstream>
 #include <memory>
+#include <vector>
 #include <tgbotxx/utils/Ptr.hpp>
 #include <tgbotxx/Exception.hpp>
 #include <iostream>
@@ -10,12 +12,20 @@
 namespace nl = nlohmann;
 
 /// @brief Helper macros to de/serialize objects
+
+/// Serialize
 #define OBJECT_SERIALIZE_FIELD(json, json_field, field) \
   json[json_field] = field;
 
 #define OBJECT_SERIALIZE_FIELD_PTR(json, json_field, field, default_value) \
   json[json_field] = field ? field->toJson() : default_value
 
+#define OBJECT_SERIALIZE_FIELD_PTR_ARRAY(json, json_field, array_field) \
+  json[json_field] = nl::json::array();                                 \
+  for(const auto& e : array_field)                                      \
+    json[json_field].push_back(e->toJson());                            \
+
+/// Deserialize
 #define OBJECT_DESERIALIZE_FIELD(json, json_field, field, default_value, optional)                                       \
   if (json.contains(json_field))                                                                                         \
   {                                                                                                                      \
@@ -51,4 +61,26 @@ namespace nl = nlohmann;
       throw Exception(err.str());                                                                                        \
     }                                                                                                                    \
     field = nullptr;                                                                                                     \
+  }
+
+#define OBJECT_DESERIALIZE_FIELD_PTR_ARRAY(json, json_field, array_field, optional)                                      \
+  if ((json.contains(json_field)) and (json[json_field].is_array()))                                                     \
+  {                                                                                                                      \
+    using T = std::remove_reference_t<std::remove_const_t<decltype(array_field)::value_type>>;                           \
+    using E = T::element_type;                                                                                           \
+    array_field.reserve(json[json_field].size());                                                                        \
+    for(const nl::json& obj : json[json_field]) {                                                                        \
+        array_field.emplace_back(new E(obj));                                                                            \
+    }                                                                                                                    \
+  }                                                                                                                      \
+  else                                                                                                                   \
+  {                                                                                                                      \
+    if(not (optional))                                                                                                   \
+    {                                                                                                                    \
+      std::ostringstream err{};                                                                                          \
+      err << __FILE__ << ':' << __LINE__ <<": "<<__PRETTY_FUNCTION__<<": Missing required field \""                      \
+      << json_field << "\" from json object: " << json.dump(2);                                                          \
+      throw Exception(err.str());                                                                                        \
+    }                                                                                                                    \
+    array_field.clear();                                                                                                 \
   }
