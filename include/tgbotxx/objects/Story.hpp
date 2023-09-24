509e33b92a47dfe02b91736e473e43a4676d0586
Bader <badereddineouaich@gmail.com>
2023-09-24 11:52:34 +0100

    added object Story object with its JSON serialization and documentation

---
+++ include/tgbotxx/objects/Story.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <tgbotxx/objects/Object.hpp>
+
+namespace tgbotxx {
+    /// @brief This object represents a message about a forwarded story in the chat.
+    /// Currently holds no information.
+    /// @ref https://core.telegram.org/bots/api#story
+    struct Story {
+        explicit Story(const nl::json& json) {
+          fromJson(json);
+        }
+
+        /// @brief Serializes this object to JSON
+        /// @returns JSON representation of this object
+        nl::json toJson() const {
+          nl::json json = nl::json::object();
+          return json;
+        }
+
+        /// @brief Deserializes this object from JSON
+        void fromJson(const nl::json &json) {
+        }
+    };
+}
