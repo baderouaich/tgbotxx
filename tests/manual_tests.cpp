9de6ea71058c0d6b92f2dcc9b6c4e640f55710b9
Bader <badereddineouaich@gmail.com>
2023-09-25 16:49:45 +0100

    updates manual_tests

--- tests/manual_tests.cpp
+++ tests/manual_tests.cpp
@@ -3,15 +3,19 @@
 using namespace tgbotxx;
 
 class MyBot : public Bot {
-
-  std::string getTestsBotToken() {
+  static std::string getTestsBotToken() {
     if(char* token = std::getenv("TESTS_BOT_TOKEN"); token != nullptr)
       return std::string(token);
      throw std::runtime_error("Couldn't find TESTS_BOT_TOKEN in the env; please export an environment variable TESTS_BOT_TOKEN with your bot token");
   }
 
 public:
-    MyBot() : Bot(getTestsBotToken()) {
+    MyBot() : Bot(getTestsBotToken()) {}
+
+private:
+    void onStart() override {
+      std::cout << __func__ << std::endl;
+
       // Drop awaiting updates (when Bot is not running, updates will remain 24 hours
       // in Telegram server before they get deleted or retrieved by BOT)
       getApi()->deleteWebhook(true);
@@ -19,23 +23,31 @@ public:
       // Register bot commands ...
       std::vector<Ptr<BotCommand>> commands;
       Ptr<BotCommand> greet(new BotCommand());
-      greet->command = "/greet";
+      greet->command = "greet";
       greet->description = "This command will greet you";
       commands.push_back(greet);
+      Ptr<BotCommand> stop(new BotCommand());
+      stop->command = "stop";
+      stop->description = "Stop the bot";
+      commands.push_back(stop);
       getApi()->setMyCommands(commands); // The above commands will be shown in the bot chat menu (bottom left)
     }
 
-public:
-    void onAnyMessage(const Ptr<Message>& message) override {
+    void onStop() override {
       std::cout << __func__ << std::endl;
-      if(message->text.starts_with("/")) { // >= C++20 (u can also use StringUtils::startsWith())
-        std::cout << "Ignoring command: " << message->text << '\n';
-        return;
-      }
+    }
+
+    void onAnyMessage(const Ptr<Message>& message) override {
+      std::cout << __func__ << ": " << message->text << std::endl;
     }
 
     void onCommand(const Ptr<Message>& command) override {
-      std::cout << __func__ << std::endl;
+      std::cout << __func__ << ": " << command->text << std::endl;
+      if(command->text == "/stop") {
+        Bot::stop();
+        return;
+      }
+
 #if api_sendMessage_implemented
     if(command->text == "start") {
         getApi()->sendMessage(command->chat->id, "Welcome " + command->from->firstName + "!");
@@ -47,14 +59,12 @@ public:
     }
 
     void onNonCommandMessage(const Ptr<Message> &message) override {
-      std::cout << __func__ << std::endl;
+      std::cout << __func__ << ": " << message->text << std::endl;
     }
-
 
     void onUnknownCommand(const Ptr<Message> &message) override {
-      std::cout << __func__ << std::endl;
+      std::cout << __func__ << ": " << message->text << std::endl;
     }
-
 };
 
 int main() {
