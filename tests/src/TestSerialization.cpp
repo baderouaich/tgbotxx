f9f658c76901414bee2629ba8a4138fe659a813c
Bader <badereddineouaich@gmail.com>
2023-09-25 17:04:52 +0100

    Fix workflow failure

--- tests/src/TestSerialization.cpp
+++ tests/src/TestSerialization.cpp
@@ -5,11 +5,12 @@ using namespace tgbotxx;
 
 TEST_CASE("Serialization & Deserialization", "User")
 {
-  User user;
-  user.id = 1444;
-  user.username = "@]qsdqsdqsd";
-  user.lastName = "wdqsd";
-  user.addedToAttachmentMenu = false;
-  user.isBot = true;
+  nl::json obj = nl::json::object();
+  obj["id"] = 1000;
+  obj["username"] = "@test";
+  obj["first_name"] = "first name";
+  obj["last_name"] = "last name";
+  obj["is_bot"] = false;
+  User user(obj);
   std::cout <<user.toJson().dump(2) << std::endl;
 }
