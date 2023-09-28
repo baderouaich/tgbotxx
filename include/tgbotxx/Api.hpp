4861c61c1fd1ac4972c39bb695ab2bfe84db7345
Bader <badereddineouaich@gmail.com>
2023-09-28 16:24:22 +0100

    implemented sendMessage Api method Use this method to send text messages. On success, the sent Message is returned.

--- include/tgbotxx/Api.hpp
+++ include/tgbotxx/Api.hpp
@@ -1,11 +1,11 @@
 #pragma once
-#include <tgbotxx/objects/Object.hpp>
 #include <tgbotxx/objects/Animation.hpp>
 #include <tgbotxx/objects/Audio.hpp>
 #include <tgbotxx/objects/BotCommand.hpp>
 #include <tgbotxx/objects/BotCommandScope.hpp>
 #include <tgbotxx/objects/CallbackGame.hpp>
 #include <tgbotxx/objects/CallbackQuery.hpp>
+#include <tgbotxx/objects/ChatAdministratorRights.hpp>
 #include <tgbotxx/objects/Chat.hpp>
 #include <tgbotxx/objects/ChatInviteLink.hpp>
 #include <tgbotxx/objects/ChatJoinRequest.hpp>
@@ -33,12 +33,18 @@
 #include <tgbotxx/objects/InlineKeyboardMarkup.hpp>
 #include <tgbotxx/objects/InlineQuery.hpp>
 #include <tgbotxx/objects/Invoice.hpp>
+#include <tgbotxx/objects/IReplyMarkup.hpp>
+#include <tgbotxx/objects/KeyboardButton.hpp>
+#include <tgbotxx/objects/KeyboardButtonPollType.hpp>
+#include <tgbotxx/objects/KeyboardButtonRequestChat.hpp>
+#include <tgbotxx/objects/KeyboardButtonRequestUser.hpp>
 #include <tgbotxx/objects/Location.hpp>
 #include <tgbotxx/objects/LoginUrl.hpp>
 #include <tgbotxx/objects/MaskPosition.hpp>
 #include <tgbotxx/objects/MessageAutoDeleteTimerChanged.hpp>
 #include <tgbotxx/objects/MessageEntity.hpp>
 #include <tgbotxx/objects/Message.hpp>
+#include <tgbotxx/objects/Object.hpp>
 #include <tgbotxx/objects/OrderInfo.hpp>
 #include <tgbotxx/objects/PassportData.hpp>
 #include <tgbotxx/objects/PassportFile.hpp>
@@ -48,6 +54,8 @@
 #include <tgbotxx/objects/PollOption.hpp>
 #include <tgbotxx/objects/PreCheckoutQuery.hpp>
 #include <tgbotxx/objects/ProximityAlertTriggered.hpp>
+#include <tgbotxx/objects/ReplyKeyboardMarkup.hpp>
+#include <tgbotxx/objects/ReplyKeyboardRemove.hpp>
 #include <tgbotxx/objects/ShippingAddress.hpp>
 #include <tgbotxx/objects/ShippingQuery.hpp>
 #include <tgbotxx/objects/Sticker.hpp>
@@ -72,6 +80,7 @@
 #include <iostream>
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
+
 namespace nl = nlohmann;
 
 namespace tgbotxx {
@@ -109,6 +118,34 @@ namespace tgbotxx {
         /// @returns true on success.
         /// @ref https://core.telegram.org/bots/api#close
         bool close() const;
+
+        /// @brief Use this method to send text messages. On success, the sent Message is returned.
+        /// @param chatId Integer Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+        /// @param text Text of the message to be sent, 1-4096 characters after entities parsing
+        /// @param disableWebPagePreview Optional. Disables link previews for links in this message
+        /// @param replyToMessageId Optional. If the message is a reply, ID of the original message
+        /// @param replyMarkup Optional. Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard, instructions to remove reply keyboard or to force a reply from the user.
+        /// @param parseMode Optional. Mode for parsing entities in the message text. See formatting options for more details. https://core.telegram.org/bots/api#formatting-options
+        /// @param disableNotification Optional. Sends the message silently. Users will receive a notification with no sound.
+        /// @param entities Optional. A JSON-serialized list of special entities that appear in message text, which can be specified instead of parse_mode
+        /// @param allowSendingWithoutReply Optional. Pass True if the message should be sent even if the specified replied-to message is not found
+        /// @param protectContent Optional. Protects the contents of the sent message from forwarding and saving
+        /// @param messageThreadId Optional. Unique identifier for the target message thread (topic) of the forum; for forum supergroups only
+        ///        One of InlineKeyboardMarkup or ReplyKeyboardMarkup or ReplyKeyboardRemove or ForceReply.
+        /// @returns sent Message object on success.
+        /// @ref https://core.telegram.org/bots/api#sendmessage
+        Ptr<Message> sendMessage(const std::int64_t& chatId,
+                                 const std::string& text,
+                                 bool disableWebPagePreview = false,
+                                 std::int64_t replyToMessageId = 0,
+                                 const Ptr<IReplyMarkup>& replyMarkup = nullptr,
+                                 const std::string& parseMode = "",
+                                 bool disableNotification = false,
+                                 const std::vector<Ptr<MessageEntity>>& entities = std::vector<Ptr<MessageEntity>>(),
+                                 bool allowSendingWithoutReply = false,
+                                 bool protectContent = false,
+                                 std::int64_t messageThreadId = 0) const;
+
         /// @brief Use this method to remove webhook integration if you decide to switch back to getUpdates.
         /// Returns True on success.
         /// @param dropPendingUpdates: Pass True to drop all pending updates.
@@ -127,13 +164,12 @@ namespace tgbotxx {
         /// @param limit Limits the number of updates to be retrieved. Values between 1-100 are accepted. Defaults to 100.
         /// @param timeout Timeout in seconds for long polling. Defaults to 0, i.e. usual short polling. Should be positive,
         /// short polling should be used for testing purposes only.
-        /// @param A JSON-serialized list of the update types you want your bot to receive.
+        /// @param allowedUpdates JSON-serialized list of the update types you want your bot to receive.
         /// For example, specify [“message”, “edited_channel_post”, “callback_query”] to only receive updates of these types.
         /// See Update for a complete list of available update types. Specify an empty list to receive all update types
         /// except chat_member (default). If not specified, the previous setting will be used.
-        /// @note: Please note that this parameter doesn't affect updates created before the call to the getUpdates,
-        /// so unwanted updates may be received for a short period of time.
         /// @returns an Array of Update objects.
+        /// @note Please note that this parameter doesn't affect updates created before the call to the getUpdates, so unwanted updates may be received for a short period of time.
         /// @note This method will not work if an outgoing webhook is set up.
         /// @note In order to avoid getting duplicate updates, recalculate offset after each server response.
         /// @link ref https://core.telegram.org/bots/api#getupdates @endlink
--- src/Api.cpp
+++ src/Api.cpp
@@ -1,71 +1,67 @@
-#include "cpr/status_codes.h"
 #include <tgbotxx/Api.hpp>
 #include <tgbotxx/Exception.hpp>
-#include <tgbotxx/objects/Update.hpp>
-#include <tgbotxx/objects/BotCommand.hpp>
-#include <tgbotxx/objects/BotCommandScope.hpp>
 #include <tgbotxx/utils/StringUtils.hpp>
 #include <utility>
-#include <cpr/cpr.h>
 using namespace tgbotxx;
 
-Api::Api(const std::string& token) : m_token(token) {
-  m_session.SetTimeout(TIMEOUT);
-  m_session.SetConnectTimeout(CONNECT_TIMEOUT);
-  m_session.SetHeader(cpr::Header{
-          {"Connection", "close"}, // disable keep-alive
-          {"Accept", "application/json"},
-          {"Content-Type", "application/x-www-form-urlencoded"},
-  });
+Api::Api(const std::string &token) : m_token(token) {
+    m_session.SetTimeout(TIMEOUT);
+    m_session.SetConnectTimeout(CONNECT_TIMEOUT);
+    m_session.SetHeader(cpr::Header{
+            {"Connection",   "close"}, // disable keep-alive
+            {"Accept",       "application/json"},
+            {"Content-Type", "application/x-www-form-urlencoded"},
+    });
 }
 
-nl::json Api::sendRequest(const std::string& endpoint, const cpr::Multipart& data) const {
-  std::ostringstream url{};
-  url << BASE_URL << "/bot" << m_token << '/' << endpoint; // workaround: token should have a prefix botTOKEN. see https://stackoverflow.com/a/41460083
-  m_session.SetUrl(cpr::Url{url.str()});
-  m_session.SetMultipart(data);
+nl::json Api::sendRequest(const std::string &endpoint, const cpr::Multipart &data) const {
+    std::ostringstream url{};
+    url << BASE_URL << "/bot" << m_token << '/'
+        << endpoint; // workaround: token should have a prefix botTOKEN. see https://stackoverflow.com/a/41460083
+    m_session.SetUrl(cpr::Url{url.str()});
+    m_session.SetMultipart(data);
 
-  bool isMultipart = not data.parts.empty();
-  if(isMultipart) // we have files?
-  {
-    m_session.UpdateHeader(cpr::Header{{
-                                               {"Content-Type", "multipart/form-data"}
-                                       }});
+    bool isMultipart = not data.parts.empty();
+    if (isMultipart) // we have files?
+    {
+        m_session.UpdateHeader(cpr::Header{{
+                                                   {"Content-Type", "multipart/form-data"}
+                                           }});
 
-  } else {
-    m_session.UpdateHeader(cpr::Header{{
-                                               {"Content-Type", "application/x-www-form-urlencoded"}
-                                       }});
-  }
-
-  cpr::Response res{};
-  if(isMultipart)
-    res = m_session.Post();
-  else
-    res = m_session.Get();
-
-  if(!res.text.compare(0, 6, "<html>")) {
-    throw Exception("Failed to get a JSON response from Telegram API. Did you enter the correct bot token?");
-  }
-
-  try {
-    nl::json response = nl::json::parse(res.text);
-    if (response["ok"].get<bool>()) {
-      return response["result"];
     } else {
-      std::string desc = response["description"];
-      if(response["error_code"] == cpr::status::HTTP_NOT_FOUND) {
-        desc += ". Did you enter the correct bot token?";
-      }
-      throw Exception(desc);
+        m_session.UpdateHeader(cpr::Header{{
+                                                   {"Content-Type", "application/x-www-form-urlencoded"}
+                                           }});
     }
-  } catch (const nl::json::exception& e) {
-    throw Exception("Failed to parse JSON response: " + res.text + "\nreason: " + e.what());
-  } catch(const Exception& e) {
-    throw; // rethrow e
-  } catch(const std::exception& e) {
-    throw; // rethrow e
-  }
+
+    cpr::Response res{};
+    if (isMultipart)
+        res = m_session.Post();
+    else
+        res = m_session.Get();
+
+    if (!res.text.compare(0, 6, "<html>")) {
+        throw Exception("Failed to get a JSON response from Telegram API. Did you enter the correct bot token?");
+    }
+
+    try {
+        nl::json response = nl::json::parse(res.text);
+        if (response["ok"].get<bool>()) {
+            return response["result"];
+        } else {
+            std::string desc = response["description"];
+            if (response["error_code"] == cpr::status::HTTP_NOT_FOUND) {
+                desc += ". Did you enter the correct bot token?";
+            }
+            throw Exception(desc);
+        }
+    } catch (const nl::json::exception &e) {
+        throw Exception("Failed to parse JSON response: " + res.text + "\nreason: " + e.what());
+    } catch (const Exception &e) {
+        throw; // rethrow e
+    } catch (const std::exception &e) {
+        throw; // rethrow e
+    }
 }
 
 Ptr<User> Api::getMe() const {
@@ -75,66 +71,69 @@ Ptr<User> Api::getMe() const {
 }
 
 bool Api::deleteWebhook(bool dropPendingUpdates) const {
-  cpr::Multipart data{};
-  if(dropPendingUpdates)
-    data.parts.emplace_back("drop_pending_updates", dropPendingUpdates);
-  return sendRequest("deleteWebhook", data).get<bool>();
+    cpr::Multipart data{};
+    if (dropPendingUpdates)
+        data.parts.emplace_back("drop_pending_updates", dropPendingUpdates);
+    return sendRequest("deleteWebhook", data);
 }
 
 /// Called every LONG_POOL_TIMEOUT seconds
-std::vector<Ptr<Update>> Api::getUpdates(std::int32_t offset, std::int32_t limit, std::int32_t timeout, const std::vector<std::string> &allowedUpdates) const {
-  cpr::Multipart data = {
-          {"offset", offset},
-          {"limit", std::max(1, std::min(100, limit))},
-          {"timeout", timeout},
-          {"allowed_updates", allowedUpdates.empty() ? "[]" : nl::json{allowedUpdates}.dump()},
-  };
-  nl::json updatesJson = sendRequest("getUpdates", data);
-  std::vector<Ptr<Update>> updates;
-  updates.reserve(updatesJson.size());
-  for(const nl::json& updateObj : updatesJson) {
-    Ptr<Update> update(new Update(updateObj));
-    updates.push_back(std::move(update));
-  }
-  return updates;
+std::vector<Ptr<Update>> Api::getUpdates(std::int32_t offset, std::int32_t limit, std::int32_t timeout,
+                                         const std::vector<std::string> &allowedUpdates) const {
+    cpr::Multipart data = {
+            {"offset",          offset},
+            {"limit",           std::max(1, std::min(100, limit))},
+            {"timeout",         timeout},
+            {"allowed_updates", allowedUpdates.empty() ? "[]" : nl::json{allowedUpdates}.dump()},
+    };
+    nl::json updatesJson = sendRequest("getUpdates", data);
+    std::vector<Ptr<Update>> updates;
+    updates.reserve(updatesJson.size());
+    for (const nl::json &updateObj: updatesJson) {
+        Ptr<Update> update(new Update(updateObj));
+        updates.push_back(std::move(update));
+    }
+    return updates;
 }
 
-bool Api::setMyCommands(const std::vector<Ptr<BotCommand>> &commands, const Ptr<BotCommandScope>& scope, const std::string &languageCode) const {
-  cpr::Multipart data{};
-  data.parts.reserve(3);
+bool Api::setMyCommands(const std::vector<Ptr<BotCommand>> &commands, const Ptr<BotCommandScope> &scope,
+                        const std::string &languageCode) const {
+    cpr::Multipart data{};
+    data.parts.reserve(3);
 
-  nl::json commandsJson = nl::json::array();
-  for(const Ptr<BotCommand>& command : commands)
-      commandsJson.push_back(command->toJson());
-  data.parts.emplace_back("commands", commandsJson.dump());
-  if (scope) {
-    data.parts.emplace_back("scope", scope->toJson().dump());
-  }
-  if (not languageCode.empty()) {
-    data.parts.emplace_back("language_code", languageCode);
-  }
+    nl::json commandsJson = nl::json::array();
+    for (const Ptr<BotCommand> &command: commands)
+        commandsJson.push_back(command->toJson());
+    data.parts.emplace_back("commands", commandsJson.dump());
+    if (scope) {
+        data.parts.emplace_back("scope", scope->toJson().dump());
+    }
+    if (not languageCode.empty()) {
+        data.parts.emplace_back("language_code", languageCode);
+    }
 
-  return sendRequest("setMyCommands", data).get<bool>();
+    return sendRequest("setMyCommands", data);
 }
 
-std::vector<Ptr<BotCommand>> Api::getMyCommands(const Ptr<BotCommandScope> &scope, const std::string &languageCode) const {
-  std::vector<Ptr<BotCommand>> commands;
-  cpr::Multipart data{};
-  data.parts.reserve(2);
-  if (scope) {
-    data.parts.emplace_back("scope", scope->toJson().dump());
-  }
-  if (not languageCode.empty()) {
-    data.parts.emplace_back("language_code", languageCode);
-  }
-  nl::json commandsJson = sendRequest("getMyCommands", data);
-  if(commandsJson.empty()) return commands;
-  commands.reserve(commandsJson.size());
-  for(const nl::json& commandObj : commandsJson) {
-    Ptr<BotCommand> cmd(new BotCommand(commandObj));
-    commands.push_back(std::move(cmd));
-  }
-  return commands;
+std::vector<Ptr<BotCommand>>
+Api::getMyCommands(const Ptr<BotCommandScope> &scope, const std::string &languageCode) const {
+    std::vector<Ptr<BotCommand>> commands;
+    cpr::Multipart data{};
+    data.parts.reserve(2);
+    if (scope) {
+        data.parts.emplace_back("scope", scope->toJson().dump());
+    }
+    if (not languageCode.empty()) {
+        data.parts.emplace_back("language_code", languageCode);
+    }
+    nl::json commandsJson = sendRequest("getMyCommands", data);
+    if (commandsJson.empty()) return commands;
+    commands.reserve(commandsJson.size());
+    for (const nl::json &commandObj: commandsJson) {
+        Ptr<BotCommand> cmd(new BotCommand(commandObj));
+        commands.push_back(std::move(cmd));
+    }
+    return commands;
 }
 
 bool Api::logOut() const {
@@ -145,3 +144,39 @@ bool Api::close() const {
     return sendRequest("close");
 }
 
+Ptr<Message> Api::sendMessage(const std::int64_t& chatId, const std::string &text,
+                              bool disableWebPagePreview, std::int64_t replyToMessageId, const Ptr<IReplyMarkup>& replyMarkup,
+                              const std::string &parseMode, bool disableNotification,
+                              const std::vector<Ptr<MessageEntity>>& entities, bool allowSendingWithoutReply,
+                              bool protectContent, std::int64_t messageThreadId) const {
+    cpr::Multipart data{};
+    data.parts.reserve(10);
+    data.parts.emplace_back("chat_id", std::to_string(chatId)); // Since cpr::Part() does not take 64bit integers (only 32bit), passing a 64bit chatId to 32bit integer gets overflown and sends wrong chat_id which causes Bad Request: chat not found
+    data.parts.emplace_back("text", text);
+    if (disableWebPagePreview)
+        data.parts.emplace_back("disable_web_page_preview", disableWebPagePreview);
+    if (replyToMessageId)
+        data.parts.emplace_back("reply_to_message_id", replyToMessageId);
+    if (replyMarkup)
+        data.parts.emplace_back("reply_markup", replyMarkup->toJson().dump());
+    if (not parseMode.empty())
+        data.parts.emplace_back("parse_mode", parseMode);
+    if (disableNotification)
+        data.parts.emplace_back("disable_notification", disableNotification);
+    if (not entities.empty()) {
+        nl::json entitiesArray = nl::json::array();
+        for(const Ptr<MessageEntity>& entity : entities)
+            entitiesArray.push_back(entity->toJson());
+        data.parts.emplace_back("entities", entitiesArray.dump());
+    }
+    if (allowSendingWithoutReply)
+        data.parts.emplace_back("allow_sending_without_reply", allowSendingWithoutReply);
+    if (protectContent)
+        data.parts.emplace_back("protect_content", protectContent);
+    if (messageThreadId)
+        data.parts.emplace_back("message_thread_id", messageThreadId);
+
+     nl::json sentMessageObj = sendRequest("sendMessage", data);
+     Ptr<Message> message(new Message(sentMessageObj));
+     return message;
+}
