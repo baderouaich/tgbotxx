af211e95f72f0b5dfa0e1188a7c0d5e305d630dc
Bader <badereddineouaich@gmail.com>
2023-09-30 13:17:41 +0100

    updated installation script

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -2,13 +2,16 @@
 # the CMake variables (CMAKE_INSTALL_BINDIR, CMAKE_INSTALL_LIBDIR, CMAKE_INSTALL_INCLUDEDIR) are set correctly
 # for those platforms. These variables are typically set by CMake to appropriate default values based on the platform.
 include(GNUInstallDirs) # Use standard CMake directory variables
-# Install header files
+
+# Install header files (our headers & lib single headers)
 install(DIRECTORY ${CMAKE_SOURCE_DIR}/include/tgbotxx DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+install(DIRECTORY ${CMAKE_SOURCE_DIR}/lib/json/include/nlohmann DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+
 # Enable position-independent code for shared libraries
 set_property(TARGET ${PROJECT_NAME} PROPERTY POSITION_INDEPENDENT_CODE ON)
 
-# Define export set and associate it with the targets we want to export (cpr & nlohmann_json)
-install(TARGETS tgbotxx cpr nlohmann_json
+# Define export set and associate it with the targets we want to export (cpr)
+install(TARGETS tgbotxx cpr
         EXPORT tgbotxx-targets
         RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
         LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
@@ -30,9 +33,7 @@ write_basic_package_version_file("${PROJECT_BINARY_DIR}/tgbotxx-config-version.c
         )
 install(
         FILES
-        ${CMAKE_SOURCE_DIR}/cmake/find-or-fetch.cmake
         ${CMAKE_SOURCE_DIR}/cmake/tgbotxx-config.cmake
-        ${CMAKE_SOURCE_DIR}/cmake/tgbotxx-dependencies.cmake
         ${PROJECT_BINARY_DIR}/tgbotxx-config-version.cmake
         DESTINATION
         ${CMAKE_INSTALL_LIBDIR}/cmake/tgbotxx
--- cmake/tgbotxx-config.cmake
+++ cmake/tgbotxx-config.cmake
@@ -1,4 +1,2 @@
 # for find_package()
-include("${CMAKE_CURRENT_LIST_DIR}/find-or-fetch.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/tgbotxx-dependencies.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/tgbotxx-targets.cmake")
