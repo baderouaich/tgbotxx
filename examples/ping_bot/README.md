48983190f6b22030d2f188cd299545eaeabaaef8
Bader <badereddineouaich@gmail.com>
2023-10-02 22:10:16 +0100

    added basic ping bot

---
+++ examples/ping_bot/README.md
@@ -0,0 +1,9 @@
+## ping_bot
+This example shows how to program a basic Telegram Bot which you can send a /ping command and it responds with pong! 
+
+### How to create a new Bot and obtain its private token ?
+1. Open the Telegram mobile app and search BotFather
+2. Send BotFather a command /newbot
+3. Follow instructions to create a new Bot
+4. After you finish the instructions, you will receive a Bot Token, make sure you keep it secured. 
+
---
+++ examples/ping_bot/main.cpp
@@ -0,0 +1,56 @@
+#include <tgbotxx/tgbotxx.hpp>
+#include <iostream>
+#include <csignal>
+using namespace tgbotxx;
+
+class PingBot : public Bot {
+public:
+  PingBot(const std::string &token) : Bot(token) {}
+
+private:
+  void onStart() override
+  {
+    // Drop pending updates
+    api()->deleteWebhook(true);
+
+    // Set my commands
+    Ptr<BotCommand> pingCmd(new BotCommand());
+    pingCmd->command = "/ping";
+    pingCmd->description = "Ping bot. You'll receive pong!";
+    api()->setMyCommands({ pingCmd });
+
+    std::cout << "Bot " << api()->getMe()->username << " Started\n";
+  }
+
+  void onCommand(const Ptr<Message> &message) override
+  {
+    if(message->text == "/ping")
+    {
+      api()->sendMessage(message->chat->id, "pong!");
+    }
+  }
+
+  void onStop() override
+  {
+    std::cout << "Bot " << api()->getMe()->username << " Stopped\n";
+  }
+};
+
+
+int main(int argc, const char *argv[]) {
+  if (argc < 2) {
+    std::cerr << "Usage:\nping_bot \"BOT_TOKEN\"\n";
+    return EXIT_FAILURE;
+  }
+
+  static std::unique_ptr<PingBot> BOT(new PingBot(argv[1]));
+  std::signal(SIGINT, [](int) { // Graceful Bot exit on CTRL+C
+    if(BOT) {
+      std::cout << "Stopping Bot. Please wait...\n";
+      BOT->stop();
+    }
+    std::exit(EXIT_SUCCESS);
+  });
+  BOT->start();
+  return EXIT_SUCCESS;
+}
