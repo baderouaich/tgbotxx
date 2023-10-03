e8449fc644a2ef5658e74195595252a10afe46fb
Bader <badereddineouaich@gmail.com>
2023-10-03 14:08:41 +0100

    Fix InlineKeyboardMarkup had wrong member data type

--- include/tgbotxx/objects/InlineKeyboardMarkup.hpp
+++ include/tgbotxx/objects/InlineKeyboardMarkup.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#include <tgbotxx/objects/IReplyMarkup.hpp>
 #include <tgbotxx/objects/Object.hpp>
+#include <tgbotxx/objects/IReplyMarkup.hpp>
 #include <tgbotxx/objects/InlineKeyboardButton.hpp>
 
 namespace tgbotxx {
@@ -13,19 +13,19 @@ namespace tgbotxx {
         }
 
         /// @brief Array of button rows, each represented by an Array of InlineKeyboardButton objects
-        std::vector<Ptr<InlineKeyboardButton>> inlineKeyboard;
+        std::vector<std::vector<Ptr<InlineKeyboardButton>>> inlineKeyboard;
 
         /// @brief Serializes this object to JSON
         /// @returns JSON representation of this object
         nl::json toJson() const override {
           nl::json json = nl::json::object();
-          OBJECT_SERIALIZE_FIELD_PTR_ARRAY(json, "inline_keyboard", inlineKeyboard);
+          OBJECT_SERIALIZE_FIELD_PTR_ARRAY_ARRAY(json, "inline_keyboard", inlineKeyboard);
           return json;
         }
 
         /// @brief Deserializes this object from JSON
         void fromJson(const nl::json &json) override {
-          OBJECT_DESERIALIZE_FIELD_PTR_ARRAY(json, "inline_keyboard", inlineKeyboard, false);
+          OBJECT_DESERIALIZE_FIELD_PTR_ARRAY_ARRAY(json, "inline_keyboard", inlineKeyboard, false);
         }
 
     private:
