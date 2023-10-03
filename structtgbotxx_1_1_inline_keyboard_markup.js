6dba6e5efd728ad36996a57a16623034e4264e2e
baderouaich <baderouaich@users.noreply.github.com>
2023-10-03 13:09:34 +0000

    docs - Fix InlineKeyboardMarkup had wrong member data type e8449fc644a2ef5658e74195595252a10afe46fb

--- structtgbotxx_1_1_inline_keyboard_markup.js
+++ structtgbotxx_1_1_inline_keyboard_markup.js
@@ -4,5 +4,5 @@ var structtgbotxx_1_1_inline_keyboard_markup =
     [ "InlineKeyboardMarkup", "structtgbotxx_1_1_inline_keyboard_markup.html#a99788b447f8f68c3c933487b4a09c138", null ],
     [ "fromJson", "structtgbotxx_1_1_inline_keyboard_markup.html#a49260a3164be4841ad61028ee90af5e6", null ],
     [ "toJson", "structtgbotxx_1_1_inline_keyboard_markup.html#afc953912b4af27ad2a8ef86349a3a5ef", null ],
-    [ "inlineKeyboard", "structtgbotxx_1_1_inline_keyboard_markup.html#a917f2347e8d9b74a0cedb1b208b7f343", null ]
+    [ "inlineKeyboard", "structtgbotxx_1_1_inline_keyboard_markup.html#aff0db6d98dc78c852a8a650be803adac", null ]
 ];
