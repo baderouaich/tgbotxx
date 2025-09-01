176a7b187c91e1edda3059fcad1f4ae3593feca5
baderouaich <baderouaich@users.noreply.github.com>
2025-09-01 18:35:34 +0000

    docs - Merge pull request #6 from thagarion/fix_response
    Fix create forum response deserialization a2b89c80017f8b82010bc37e6a7b1c6a3b0fc243

--- structtgbotxx_1_1_forum_topic.js
+++ structtgbotxx_1_1_forum_topic.js
@@ -5,7 +5,7 @@ var structtgbotxx_1_1_forum_topic =
     [ "fromJson", "structtgbotxx_1_1_forum_topic.html#af57fb2e2ef0e4f7bcb4673326eaff841", null ],
     [ "toJson", "structtgbotxx_1_1_forum_topic.html#aaf9d40fb219f98fc0e8b6f8bc7db4564", null ],
     [ "iconColor", "structtgbotxx_1_1_forum_topic.html#a725ea3071c4ac5f08ea00f30e52fef8f", null ],
-    [ "iconCustomEmojiId", "structtgbotxx_1_1_forum_topic.html#a44d71c44f667c27068598dfefed1c744", null ],
+    [ "iconCustomEmojiId", "structtgbotxx_1_1_forum_topic.html#affc6c06efd628df72b66ad0e860f8d7c", null ],
     [ "messageThreadId", "structtgbotxx_1_1_forum_topic.html#a57b74e718df2d034f0d8d7df8fec9dcb", null ],
     [ "name", "structtgbotxx_1_1_forum_topic.html#aedc5e544671009a805d5198052d6d988", null ]
 ];
